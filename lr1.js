169738fc335c0b45d5a3334779bf1c89dd51ca0f
programmist <programmist.030@gmail.com>
2025-02-25 20:23:06 +0400

    initial commit

---
+++ lr1.js
@@ -0,0 +1,38 @@
+function isNonHypotenuse(n) {
+    // 2 is a prime number but
+    // not of the form 4k+1
+    // so, keep Dividing n by 2
+    // until n is divisible by 2
+    while (n % 2 === 0) {
+        n = n / 2;
+    }
+
+    // n must be odd at this point. So we can skip
+    // one element (Note i = i +2)
+    for (i = 3; i <= sqrt(n); i = i + 2) {
+
+        // if i divides n
+        // check if i is of the form
+        // 4k+1 or not
+
+        if (n % i == 0) {
+            if ((i - 1) % 4 == 0)
+                return false;
+
+            // while i divides n
+            // divide n by i
+            // and update n
+            while (n % i === 0) {
+                n = n / i;
+            }
+        }
+    }
+
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2 && (n - 1) % 4 == 0)
+        return false;
+
+    else
+        return true;
+}
